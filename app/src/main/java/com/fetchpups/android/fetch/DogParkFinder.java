e5bb41a22c1afe43bb102b90fde95fa22cd11045
James Albu <James Albu>
2017-03-09 01:57:06 -0500

    added implementation of dogParkMap

---
+++ app/src/main/java/com/fetchpups/android/fetch/DogParkFinder.java
@@ -0,0 +1,13 @@
+package com.fetchpups.android.fetch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DogParkFinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_park_finder);
+    }
+}
--- app/src/main/java/com/fetchpups/android/fetch/DogParkMapsFragment.java
+++ app/src/main/java/com/fetchpups/android/fetch/DogParkMapsFragment.java
@@ -1,11 +1,20 @@
 package com.fetchpups.android.fetch;
 
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -15,14 +24,19 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.net.URI;
 
-public class DogParkMapsFragment extends Fragment {
+import static android.content.Context.LOCATION_SERVICE;
+
+
+public class DogParkMapsFragment extends Fragment implements LocationListener {
 
     MapView mMapView;
     private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.location_fragment, container, false);
 
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
@@ -35,7 +49,13 @@ public class DogParkMapsFragment extends Fragment {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        Criteria criteria = new Criteria();
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(provider);
+        final double latitude = location.getLatitude();
+        final double longitude = location.getLongitude();
+        /*  This code contains a map fragment - meant to show on the bottom of the current view
         mMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap mMap) {
@@ -45,7 +65,7 @@ public class DogParkMapsFragment extends Fragment {
 //                googleMap.setMyLocationEnabled(true);
 
                 // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
+                LatLng sydney = new LatLng(latitude, longitude);
                 googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
 
                 // For zooming automatically to the location of the marker
@@ -53,9 +73,17 @@ public class DogParkMapsFragment extends Fragment {
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
+        */
 
+        //Code that takes current location and searches for nearby dog parks
+        Uri gmmIntentUri = Uri.parse("geo:"+latitude+","+longitude+"?q=dog parks");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
         return rootView;
     }
+    @Override
+    public void onLocationChanged(Location location) {}
 
     @Override
     public void onResume() {
