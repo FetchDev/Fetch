8d33e81585a93d8556277124022861ea2d78e205
Eduardo <gonzalez14@mail.usf.edu>
2017-03-20 17:14:34 -0400

    Working map fragment with permissions.

--- app/src/main/java/com/fetchpups/android/fetch/DogParkMapsFragment.java
+++ app/src/main/java/com/fetchpups/android/fetch/DogParkMapsFragment.java
@@ -1,20 +1,31 @@
 package com.fetchpups.android.fetch;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -22,18 +33,21 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import static android.content.Context.LOCATION_SERVICE;
 import static com.fetchpups.android.fetch.R.id.map;
 
 
-public class DogParkMapsFragment extends Fragment implements LocationListener {
+public class DogParkMapsFragment extends Fragment implements LocationListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     //Map & Location Data
-    MapView mMapView;
     private GoogleMap googleMap;
-    LocationManager locationManager;
+    private MapView mMapView;
+    private LocationRequest mLocationRequest;
+    private Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
 
     //Set default permissions to USF classroom
     double latitude     = 28.058412;
@@ -42,12 +56,14 @@ public class DogParkMapsFragment extends Fragment implements LocationListener {
     //ID for the permission request
     private static final int READ_LOCATION_PERMISSIONS_REQUEST = 1;
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.location_fragment, container, false);
 
-        getLocationPermission();
+//        getLocationPermission();
 
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
@@ -60,48 +76,111 @@ public class DogParkMapsFragment extends Fragment implements LocationListener {
             e.printStackTrace();
         }
 
+        mMapView.getMapAsync(this);
 
-
+        //Old getMapAsync code
         //This code contains a map fragment - meant to show on the bottom of the current view
-        mMapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                googleMap = mMap;
+//        mMapView.getMapAsync(new OnMapReadyCallback() {
+//            @Override
+//            public void onMapReady(GoogleMap mMap) {
+//                googleMap = mMap;
+//
+//
+//                // For showing a move to my location button
+////                googleMap.setMyLocationEnabled(true);
+//
+//
+//
+//                // For dropping a marker at a point on the Map
+//                LatLng currentLocation = new LatLng(latitude, longitude);
+//                googleMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location").snippet("Marker Description"));
+//
+//                // For zooming automatically to the location of the marker
+//                CameraPosition cameraPosition = new CameraPosition.Builder().target(currentLocation).zoom(12).build();
+//                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//            }
+//
+//
+//        });
 
 
-                // For showing a move to my location button
-//                googleMap.setMyLocationEnabled(true);
-
-
-
-                // For dropping a marker at a point on the Map
-                LatLng currentLocation = new LatLng(latitude, longitude);
-                googleMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location").snippet("Marker Description"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(currentLocation).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
-
-
-        });
-
-
-        //Code that takes current location and searches for nearby dog parks
-//        Uri gmmIntentUri = Uri.parse("geo:"+latitude+","+longitude+"?q=dog parks");
-//        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-//        mapIntent.setPackage("com.google.android.apps.maps");
-//        startActivity(mapIntent);
 
         return rootView;
     }
 
-    public void ProcessingLocation(){
-        locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
-        Location location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
+    @Override
+    public void onMapReady(GoogleMap mMap) {
+        googleMap = mMap;
+
+
+        //Initialize Google Play Services
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                //Location permission already granted
+                buildGoogleApiClient();
+                googleMap.setMyLocationEnabled(true);
+            } else{
+                //Request location permission since it hasn't been granted (yet)
+                getLocationPermission();
+            }
+        } else {
+            buildGoogleApiClient();
+            googleMap.setMyLocationEnabled(true);
+        }
+
+//        // For dropping a marker at a point on the Map
+//        LatLng currentLocation = new LatLng(latitude, longitude);
+//        googleMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location").snippet("Marker Description"));
+//
+//        // For zooming automatically to the location of the marker
+//        CameraPosition cameraPosition = new CameraPosition.Builder().target(currentLocation).zoom(12).build();
+//        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if(mLastLocation != null){
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+
+            LatLng currLoc = new LatLng(latitude, longitude);
+            googleMap.addMarker(new MarkerOptions().position(currLoc).title("Current Location"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currLoc, 11));
+        }
+
+
+//        mLocationRequest = new LocationRequest();
+//        mLocationRequest.setInterval(1000);
+//        mLocationRequest.setFastestInterval(1000);
+//        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+//        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+//            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+//        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    protected synchronized void buildGoogleApiClient(){
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
 
 
     @Override
@@ -121,6 +200,11 @@ public class DogParkMapsFragment extends Fragment implements LocationListener {
     @Override
     public void onPause() {
         super.onPause();
+
+        //Stop location updates when Fragment no longer active
+        if(mGoogleApiClient != null){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
         mMapView.onPause();
     }
 
@@ -138,20 +222,29 @@ public class DogParkMapsFragment extends Fragment implements LocationListener {
 
     //Called when the user performs an action that requires the user's location
     public void getLocationPermission(){
-        //1. Use support library version ContextCompat.checkSelfPermission() to avoid checking the build version
-        //2. Always check for permission (even after granted) since a user can revoke perms at any time
+        // Always check for permission (even after granted) since a user can revoke perms at any time
         if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
             //Permission not already granted
             //Check if user has been asked about this permission already and denied it. If so, give explanation for permission usage
             if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
-                //TODO: Show some UI to explain reasoning. Probably a Toast.
-
+                //Show some UI to explain reasoning. Probably a Toast.
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("location Permission Required")
+                        .setMessage("This app requires the Location permission for proper functionality. Please accept to enable this functionality")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //Prompt the user once explanation has been shown
+                                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, READ_LOCATION_PERMISSIONS_REQUEST);
+                            }
+                        })
+                        .create().show();
+            } else {
+                //No explanation necessary, we can just request the permission
+                //Displays standard permission request UI
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, READ_LOCATION_PERMISSIONS_REQUEST);
             }
-            //Fire off async request to actually get the permission
-            //Displays standard permission request UI
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, READ_LOCATION_PERMISSIONS_REQUEST);
         }
-
     }
 
     //Callback with the request from calling requestPermission
@@ -165,10 +258,14 @@ public class DogParkMapsFragment extends Fragment implements LocationListener {
                 //If request cancelled, results[] is empty
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //TODO: (Permission granted) Do location related work here
-                    ProcessingLocation();
+                    if(mGoogleApiClient == null){
+                        buildGoogleApiClient();
+                    }
+                    googleMap.setMyLocationEnabled(true);
                 } else {
                     //(Permission denied). Disable functionality that depends on this permission
                     //TODO: Set longitude/latitude to some default values before this?
+                    Toast.makeText(getActivity(), "Permission denied", Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
