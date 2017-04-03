b8969b4c979a0fef778ca22d62d317d4c508a7fe
Eduardo <gonzalez14@mail.usf.edu>
2017-04-02 21:26:55 -0400

    Functional merge of firstChanges and ApiHandlers

--- app/src/main/java/com/fetchpups/android/fetch/News.java
+++ app/src/main/java/com/fetchpups/android/fetch/News.java
@@ -9,6 +9,8 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.fetchpups.android.fetch.utils.ApiHandler;
+
 public class News extends Fragment {
 
     //TODO: use variable to differentiate between local areas or national - default Tampa
@@ -18,22 +20,23 @@ public class News extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.news, container, false);
         mWebView = (WebView) v.findViewById(R.id.webview);
+        String pageUrl = ApiHandler.getCityPrefUrl(getActivity(), 5);
 
-        switch (area){
-            case "st_Pete":
-                mWebView.loadUrl("https://www.fetchpups.com/local-scoop-1/");
-                break;
-            case "clearwater":
-                mWebView.loadUrl("https://www.fetchpups.com/local-scoop-2/");
-                break;
-            case "orlando":
-                mWebView.loadUrl("https://www.fetchpups.com/local-scoop-3/");
-                break;
-            case "tampa":
-            default:
-                mWebView.loadUrl("https://www.fetchpups.com/local-scoop/");
-                break;
-        }
+//        switch (area){
+//            case "st_Pete":
+//                mWebView.loadUrl("https://www.fetchpups.com/local-scoop-1/");
+//                break;
+//            case "clearwater":
+//                mWebView.loadUrl("https://www.fetchpups.com/local-scoop-2/");
+//                break;
+//            case "orlando":
+//                mWebView.loadUrl("https://www.fetchpups.com/local-scoop-3/");
+//                break;
+//            case "tampa":
+//            default:
+//                mWebView.loadUrl("https://www.fetchpups.com/local-scoop/");
+//                break;
+//        }
 
 
         //Enable javascript ---not sure if needed
@@ -42,6 +45,7 @@ public class News extends Fragment {
 
         //Force links and redirects to open nin the webview
         mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl(pageUrl);
 
         return v;
     }
